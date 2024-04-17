be82100d39ddfb166775efb3fd9e5eab06deaf26
arifadnan05 <arifahamedadnan@gmail.com>
2024-04-17 23:29:53 +0600

    Error Page Added

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -15,7 +15,7 @@ const Error = () => {
                             <span className="sr-only">Error</span>404
                         </h2>
                         <p className="text-2xl font-semibold md:text-3xl">Sorry, we could not find this page.</p>
-                        <p className="mt-4 mb-8 font-semibold dark:text-gray-600">But dont worry, you can find plenty of other things on our homepage.</p>
+                        <p className="mt-4 mb-8 font-semibold dark:text-gray-600">But don't worry, you can find plenty of other things on our homepage.</p>
                          <NavLink  className="btn btn-secondary px-8 py-3 font-semibold rounded dark:bg-default-600 ">Back to homepage</NavLink> 
                     </div>
                 </div>
