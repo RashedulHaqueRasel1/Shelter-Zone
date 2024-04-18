d4e6902fd483a01eff51a97231da2ce7e5ef11e9
arifadnan05 <arifahamedadnan@gmail.com>
2024-04-18 09:38:43 +0600

    API Hidden (Assignment Done)

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAUj8ohhROJdtQENNdCVchteEOisSj0ZHo",
-  authDomain: "residential-a9.firebaseapp.com",
-  projectId: "residential-a9",
-  storageBucket: "residential-a9.appspot.com",
-  messagingSenderId: "322274162198",
-  appId: "1:322274162198:web:74b1ab63bf1bde56932737"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
